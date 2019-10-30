b302304df1980029a20bf7e69b64ab69b997addf
Eva van Geenen <eva.vangeenen@incentro.com>
2019-10-30 13:29:59 +0100

    Aspect class with Pointcut declarations and before advice

---
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,43 @@
+package com.luv2code.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	//POINTCUT DECLARATIONS
+	
+	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
+	private void forDAOPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
+	private void forAppFlow() {}
+	
+	
+	//ADVICES
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		String method = theJoinPoint.getSignature().toShortString();
+		
+		myLogger.info(method);
+	}
+	
+}
